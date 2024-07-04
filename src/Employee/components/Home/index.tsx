0a229c9a73d12e5be48f278ca57166f015edcadb
datnt286 <datnt286@gmail.com>
2024-07-04 20:23:28 +0700

    add total sold product

--- src/Employee/components/Home/index.tsx
+++ src/Employee/components/Home/index.tsx
@@ -1,6 +1,47 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AxiosInstance from '../../../services/AxiosInstance';
+import Swal from 'sweetalert2';
 
 const Home: React.FC = () => {
+    const [report, setReport] = useState({
+        totalRevenue: 0,
+        totalSoldProducts: 0,
+        totalCustomers: 0,
+        totalEmployees: 0,
+        topSellingProducts: [],
+    });
+
+    const fetchReport = async () => {
+        try {
+            const response = await AxiosInstance.get('/Report');
+
+            if (response.status === 200) {
+                setReport({
+                    totalRevenue: response.data.totalRevenue,
+                    totalSoldProducts: response.data.totalSoldProducts,
+                    totalCustomers: response.data.totalCustomers,
+                    totalEmployees: response.data.totalEmployees,
+                    topSellingProducts: response.data.topSellingProducts,
+                });
+            }
+        } catch (error) {
+            console.error('Lỗi khi tải dữ liệu: ', error);
+
+            Swal.fire({
+                title: 'Lỗi khi tải dữ liệu!',
+                icon: 'error',
+                toast: true,
+                position: 'top-end',
+                showConfirmButton: false,
+                timer: 3000,
+            });
+        }
+    };
+
+    useEffect(() => {
+        fetchReport();
+    }, []);
+
     return (
         <>
             <div className="mt-3">
@@ -8,62 +49,60 @@ const Home: React.FC = () => {
                     <div className="col-lg-3 col-6">
                         <div className="small-box bg-info">
                             <div className="inner">
-                                <h3>150</h3>
+                                <h3>{report.totalSoldProducts}</h3>
 
-                                <p>New Orders</p>
+                                <p>Sản phẩm đã bán</p>
                             </div>
                             <div className="icon">
                                 <i className="ion ion-bag"></i>
                             </div>
                             <a href="#" className="small-box-footer">
-                                More info <i className="fas fa-arrow-circle-right"></i>
+                                Xem thêm <i className="fas fa-arrow-circle-right"></i>
                             </a>
                         </div>
                     </div>
                     <div className="col-lg-3 col-6">
                         <div className="small-box bg-success">
                             <div className="inner">
-                                <h3>
-                                    53<sup style={{ fontSize: '20px' }}>%</sup>
-                                </h3>
+                                <h3>{report.totalRevenue.toLocaleString() + ' ₫'}</h3>
 
-                                <p>Bounce Rate</p>
+                                <p>Tổng doanh thu</p>
                             </div>
                             <div className="icon">
                                 <i className="ion ion-stats-bars"></i>
                             </div>
                             <a href="#" className="small-box-footer">
-                                More info <i className="fas fa-arrow-circle-right"></i>
+                                Xem thêm <i className="fas fa-arrow-circle-right"></i>
                             </a>
                         </div>
                     </div>
                     <div className="col-lg-3 col-6">
                         <div className="small-box bg-warning">
                             <div className="inner">
-                                <h3>44</h3>
+                                <h3>{report.totalCustomers}</h3>
 
-                                <p>User Registrations</p>
+                                <p>Tổng khách hàng</p>
                             </div>
                             <div className="icon">
                                 <i className="ion ion-person-add"></i>
                             </div>
                             <a href="#" className="small-box-footer">
-                                More info <i className="fas fa-arrow-circle-right"></i>
+                                Xem thêm <i className="fas fa-arrow-circle-right"></i>
                             </a>
                         </div>
                     </div>
                     <div className="col-lg-3 col-6">
                         <div className="small-box bg-danger">
                             <div className="inner">
-                                <h3>65</h3>
+                                <h3>{report.totalEmployees}</h3>
 
-                                <p>Unique Visitors</p>
+                                <p>Tổng nhân viên</p>
                             </div>
                             <div className="icon">
                                 <i className="ion ion-pie-graph"></i>
                             </div>
                             <a href="#" className="small-box-footer">
-                                More info <i className="fas fa-arrow-circle-right"></i>
+                                Xem thêm <i className="fas fa-arrow-circle-right"></i>
                             </a>
                         </div>
                     </div>
